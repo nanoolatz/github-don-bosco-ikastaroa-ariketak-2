1a24852768087ab1890800c57771c5a351ce9534
Olatz <o.idigoras@fpdonbosco.com>
2018-09-25 16:41:54 +0200

    blink-a zuzenduta setup eta read

--- blink.ino
+++ blink.ino
@@ -1,9 +1,9 @@
-void setups() {
+void setup() {
   pinMode(LED_BUILTIN, INPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalRead(LED_BUILTIN, LOW);
   sleep(1000);
   digitalRead(LED_BUILTIN, HIGH);
   sleep(2000);
